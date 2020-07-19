1ee43a990fb263b97fc544dd82c6efd228f13fd1
legenddaniel <siyuanincanada@gmail.com>
2020-07-19 12:51:27 -0400

    Updates

---
+++ precache-manifest.96e8060c8dd2ab0c3f6438c2661d7996.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "227851775c3c8d15dab51de347ed3586",
+    "url": "/profile-list/index.html"
+  },
+  {
+    "revision": "2396101ff48602b80eb6",
+    "url": "/profile-list/static/css/main.cb7d5e00.chunk.css"
+  },
+  {
+    "revision": "5d9d253a0105e4697083",
+    "url": "/profile-list/static/js/2.ecf7f0d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/profile-list/static/js/2.ecf7f0d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2396101ff48602b80eb6",
+    "url": "/profile-list/static/js/main.adf5e9a7.chunk.js"
+  },
+  {
+    "revision": "29a03aa67bb5f6bfc34c",
+    "url": "/profile-list/static/js/runtime-main.b0b45d26.js"
+  }
+]);
